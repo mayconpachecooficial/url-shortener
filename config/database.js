74ffb2c3a39e261d765d84d069e6b0f2408780b9
MAYCON PACHECO <151109977+mayconpachecooficial@users.noreply.github.com>
2024-09-11 15:31:31 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  logging: false // Defina como `true` para depuração
+  logging: false 
 });
 
 module.exports = sequelize;
